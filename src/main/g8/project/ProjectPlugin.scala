f52e5d967df1064946186a032c4f8da6ae6ecce8
AesaKamar <aesa.kamar@gmail.com>
2017-05-16 10:20:12 -0400

    Integration tests with docker compose up from sbt

--- src/main/g8/project/ProjectPlugin.scala
+++ src/main/g8/project/ProjectPlugin.scala
@@ -5,6 +5,7 @@ import sbt.Keys._
 
 import scoverage.ScoverageKeys._
 import wartremover._
+import sbtbuildinfo.BuildInfoPlugin, BuildInfoPlugin.autoImport._
 
 /** Example of how to apply settings across a build (difficult to do in build.sbt) */
 object ProjectPlugin extends AutoPlugin {
@@ -22,6 +23,16 @@ object ProjectPlugin extends AutoPlugin {
 
   // NOTE: everything in here is applied to every project (a better `commonSettings`)
   override val projectSettings = Seq(
+    // TODO (sbt-cake#8) When that is done, we can remove it from here
+    scalacOptions ++= Seq("-Ypartial-unification"),
+    // we have multiple microservices in this project
+    buildInfoPackage := s"$organisation_domain$.$organisation$.$name$.build",
+    buildInfoKeys := Seq[BuildInfoKey](
+      name,
+      version,
+      scalaVersion,
+      sbtVersion
+    )
   )
 
 }
