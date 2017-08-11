3a20a37db5a61cf0216b05ecc07990c8c37f6108
Ferhat Aydın <ferhtaydn@gmail.com>
2017-08-11 15:06:41 +0100

    CO-214: fix buildInfoPackage setting key

--- src/main/g8/project/ProjectPlugin.scala
+++ src/main/g8/project/ProjectPlugin.scala
@@ -22,7 +22,7 @@ object ProjectPlugin extends AutoPlugin {
   override val buildSettings = Seq(
     name := "$name$",
     organization := "$organisation_domain$.$organisation$",
-    buildInfoPackage := "net.cakesolutions.akkarepo.build",
+    buildInfoPackage := "$organisation_domain$.$organisation$.$name$.build",
     buildInfoKeys := Seq[BuildInfoKey](
       name,
       version,
